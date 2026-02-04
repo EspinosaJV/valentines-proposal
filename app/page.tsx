d324721fdc990498e9f410115ce75c96c55f9d5b
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2026-02-04 16:58:40 +0800

    feat: Implemented Photo Gallery Background for both Desktop & Mobile views

--- app/page.tsx
+++ app/page.tsx
@@ -29,34 +29,32 @@ export default function Home() {
   // Show Loading Screen
   if (viewState === "loading") {
     return (
-      <main className="min-h-screen flex flex-col items-center justify-center bg-[#1E1E1E] relative overflow-hidden">
-        {/* Background Overlay */}
-        <div className="absolute inset-0 bg-valentine-brand/5 mix-blend-overlay"></div>
-
-        {/* Floating Heart Animation */}
-        <div className="relative z-10 animate-bounce mb-6">
-          <img
-            src="/assets/center-heart-icon.png"
-            alt="Loading Heart"
-            className="w-24 h-24 object-contain drop-shadow-xl"
+      <main className="min-h-screen flex flex-col items-center justify-center relative overflow-hidden bg-valentine-surface">
+        {/* BACKGROUND IMADES */}
+        <div className="absolute inset-0 z-0">
+          <div className="absolute inset-0 bg-cover bg-center bg-[url('/assets/bg-mobile-collage.jpg')] md:bg-[url('/assets/bg-desktop-collage.jpg')]"
           />
+          <div className="absolute inset-0 bg-valentine-brand/40 mix-blend-overlay md:bg-valentine-brand/30 md:mix-blend-multiply"></div>
+          <div className="absolute inset-0 bg-black/40"></div>
         </div>
 
-        {/*Main Text */}
-        <h1
-          className="relative z-10 font-bold text-4xl md:text-5xl text-[#C08081] tracking-widest uppercase drop-shadow-lg text-center"
-          style={{ fontFamily: '"Playfair Display", serif' }}
-        >
-          WELCOME BABI!!!
-        </h1>
+        {/* CONTENT */}
+        <div className="relative z-10 flex flex-col items-center p-4">
+          {/* FLOATING HEART ANIMATION */}
+          <div className="animate-bounce mb-6">
+            <img src="/assets/center-heart-icon.png" alt="Loading Heart" className="w-24 h-24 object-contain drop-shadow-xl"/>
+          </div>
 
-        {/* Subtext */}
-        <p 
-          className="relative z-10 mt-4 text-white/60 text-lg tracking-wider animate-pulse font-light"
-          style={{ fontFamily: '"Lato", sans-serif' }}
-        >
-          Loading our memories together...
-        </p>
+          {/* MAIN TEXT */}
+          <h1 className="font-bold text-4xl md:text-5xl text-white tracking-widest uppercase drop-shadow-2xl text-center" style={{ fontFamily: '"Playfair Display", serif' }}>
+            Access Granted
+          </h1>
+
+          {/* SUBTEXT */}
+          <p className="mt-4 text-white/90 text-lg tracking-wider animate-pulse font-light drop-shadow-md text-center" style={{ fontFamily: '"Lato", sans-serif' }}>
+          Loading our memories...
+          </p>
+        </div>
       </main>
     );
   }
